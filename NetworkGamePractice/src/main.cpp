f5c8f0d651b5a49fd3d21c5ae8dbd889a3581e8c
mmj4594 <csmmj4594@postech.ac.kr>
2025-01-08 21:14:58 +0900

    VAO/VBO/EBO 추가하기

--- NetworkGamePractice/src/main.cpp
+++ NetworkGamePractice/src/main.cpp
@@ -2,8 +2,9 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
+constexpr int SCREEN_WIDTH = 800;
+constexpr int SCREEN_HEIGHT = 600;
+constexpr int VERTEX_SIZE = 3;
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
@@ -16,6 +17,40 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
+}
+
+// Vertex Data (Square)
+float squareVertices[] = {
+	// x, y, z
+	-0.5f, -0.5f, 0.0f,  // Bottom-left
+	 0.5f, -0.5f, 0.0f,  // Bottom-right
+	 0.5f,  0.5f, 0.0f,  // Top-right
+	-0.5f,  0.5f, 0.0f   // Top-left
+};
+unsigned int squareIndices[] = {
+	0, 1, 2, // Triangle 1
+	2, 3, 0  // Triangle 2
+};
+
+void setupBuffers(unsigned int& VAO, unsigned int& VBO, unsigned int& EBO)
+{
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(squareVertices), squareVertices, GL_STATIC_DRAW);
+
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(squareIndices), squareIndices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, VERTEX_SIZE, GL_FLOAT, GL_FALSE, VERTEX_SIZE * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// Unbind
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 }
 
 int main()
@@ -51,7 +86,13 @@ int main()
 		glfwTerminate();
 		return -1;
 	}
+
+	// Set Viewport Size
 	glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+	// VAO/VBO/EBO Setting
+	unsigned int VAO, VBO, EBO;
+	setupBuffers(VAO, VBO, EBO);
 
 	// Main loop
 	while (!glfwWindowShouldClose(window))
@@ -59,7 +100,12 @@ int main()
 		glfwPollEvents();
 
 		// Clear Screen
+		glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		// VAO Test
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		// Buffer Swap
 		glfwSwapBuffers(window);
