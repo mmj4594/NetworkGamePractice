f1d2f3ffaafb2a7cf477fa7848324fbf56afc906
mmj4594 <csmmj4594@postech.ac.kr>
2025-02-09 23:47:21 +0900

    WSA를 이용하여 서버와 클라이언트 사이의 연결 구현
    - Localhost:9000 이용

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -4,46 +4,91 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <string>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
 #include "Graphics.h"
 #include "Game.h"
 
+#pragma comment(lib, "ws2_32.lib")
+
 int main()
 {
-	if (Graphics::Get().initializeGraphics() == false)
+	// Connnect to Server
+	WSADATA wsaData;
+	SOCKET clientSocket;
+	struct sockaddr_in serverAddr;
 	{
-		std::cerr << "Failed to initialize Graphics!" << std::endl;
-		return -1;
+		// Windows Socket API Startup
+		WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+		// Cretae Client Socket(IPv4, TCP) and Set Target Server Address
+		clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+		serverAddr.sin_family = AF_INET;
+		serverAddr.sin_port = htons(9000);
+		const char* serverIPAddr = "127.0.0.1";
+		inet_pton(AF_INET, serverIPAddr, &serverAddr.sin_addr);
+
+		// Try to Connect to Server
+		if (connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+		{
+			std::cerr << "Failed to Connect to Server!" << std::endl;
+			return 1;
+		}
+		std::cout << "Connected to Server!" << std::endl;
+
+		// Send Message to Server
+		const char* message = "Hello, Server!";
+		send(clientSocket, message, static_cast<int>(strlen(message)), 0);
+
+		// Receive Message from Server
+		char buffer[1024] = { 0 };
+		recv(clientSocket, buffer, sizeof(buffer), 0);
+		std::cout << "Message from Server: " << buffer << std::endl;
 	}
 
-	Game::Get().beginPlay();
-
-	// Main loop
-	while (!Graphics::Get().shouldClose())
+	// Start Game
 	{
-		static double tickTimer = 0.0;
-		static double renderTimer = 0.0;
-
-		static double previousTime = 0.0;
-		const double currentTime = glfwGetTime();
-		const double elapsedTime = currentTime - previousTime;
-		previousTime = currentTime;
-
-		tickTimer += (elapsedTime * Game::Get().currentTimeScale);
-		renderTimer += elapsedTime;
-
-		while (tickTimer >= FRAME_TIME)
+		if (Graphics::Get().initializeGraphics() == false)
 		{
-			Game::Get().tick(static_cast<float>(tickTimer));
-			tickTimer -= FRAME_TIME;
+			std::cerr << "Failed to initialize Graphics!" << std::endl;
+			return -1;
 		}
 
-		while (renderTimer >= FRAME_TIME)
+		Game::Get().beginPlay();
+
+		// Main loop
+		while (!Graphics::Get().shouldClose())
 		{
-			Graphics::Get().renderFrame(static_cast<float>(renderTimer));
-			renderTimer -= FRAME_TIME;
+			static double tickTimer = 0.0;
+			static double renderTimer = 0.0;
+
+			static double previousTime = 0.0;
+			const double currentTime = glfwGetTime();
+			const double elapsedTime = currentTime - previousTime;
+			previousTime = currentTime;
+
+			tickTimer += (elapsedTime * Game::Get().currentTimeScale);
+			renderTimer += elapsedTime;
+
+			while (tickTimer >= FRAME_TIME)
+			{
+				Game::Get().tick(static_cast<float>(tickTimer));
+				tickTimer -= FRAME_TIME;
+			}
+
+			while (renderTimer >= FRAME_TIME)
+			{
+				Graphics::Get().renderFrame(static_cast<float>(renderTimer));
+				renderTimer -= FRAME_TIME;
+			}
 		}
 	}
 
 	glfwTerminate();
+
+	// Close Sockets
+	closesocket(clientSocket);
+	WSACleanup();
+
 	return 0;
 }
--- Server/src/Main.cpp
+++ Server/src/Main.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
+#include <winsock2.h>
+
+#pragma comment(lib, "ws2_32.lib")
 
 int main()
 {
-	std::cout << "Hello World" << std::endl;
+	WSADATA wsaData;
+	SOCKET serverSocket, clientSocket;
+	struct sockaddr_in serverAddr, clientAddr;
+	int clientAddrSize = sizeof(clientAddr);
+
+	// Windows Socket API Startup
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	// Create Server Socket(IPv4, TCP)
+	serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = INADDR_ANY;
+	serverAddr.sin_port = htons(9000);
+	bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+
+	// Wait for 1 Client
+	listen(serverSocket, 1);
+	std::cout << "Waiting for Client" << std::endl;
+
+	// Accept Client
+	clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &clientAddrSize);
+	std::cout << "Accepted Client!" << std::endl;
+
+	// Receive Message from Client
+	char buffer[1024] = { 0 };
+	recv(clientSocket, buffer, sizeof(buffer), 0);
+	std::cout << "Message from Client: " << buffer << std::endl;
+
+	// Send Message to Client
+	const char* message = "Hello, Client!";
+	send(clientSocket, message, static_cast<int>(strlen(message)), 0);
+
+	// Close Sockets
+	closesocket(clientSocket);
+	closesocket(serverSocket);
+	WSACleanup();
+
 	return 0;
 }
